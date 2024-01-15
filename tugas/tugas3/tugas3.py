dde656d4fe174122e8a768b925b10d99cff6e22d
nitroz23 <therealnitroz23@gmail.com>
2024-01-15 18:40:59 +0700

    add image to tugas3

--- tugas/tugas3/tugas3.py
+++ tugas/tugas3/tugas3.py
@@ -1,4 +1,9 @@
 import cv2
 import numpy as np
 
-# Tulis Kodingan kalian dibawah
+img = cv2.imread("tugas/tugas3/shapes.jpg")
+cv2.imshow("Original", img)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
